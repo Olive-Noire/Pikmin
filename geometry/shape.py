c77b34ef407188ea7724801802a20bb87ff600a3
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-01-10 15:27:58 +0100

    shape.py
    Mis à jour de quelques trucs.

--- geometry/shape.py
+++ geometry/shape.py
@@ -1,7 +1,7 @@
+from geometry.line import *
 from geometry.vector import *
 from geometry.utils import squared_distance, barycenter
 from math import sin,cos,tan,atan,sqrt,pi
-
 
 class Shape:
     def __init__(self):
@@ -11,31 +11,84 @@ class Circle(Shape):
     def __init__(self, center: Point, radius: float):
         assert(radius > 0)
 
-        self.center = center
-        self.radius = radius
+        self._center = center
+        self._radius = radius
     
     def get_center(self):
-        return self.center
+        return self._center
     
     def get_radius(self):
-        return self.radius
+        return self._radius
     
     def set_center(self, center):
-        self.center = center
+        self._center = center
     
     def set_radius(self, radius):
         assert(radius > 0)
-        self.radius = radius
-
+        self._radius = radius
+    
     def __and__(self, other):
         if type(other) == Line:
             a, b, c = other.get_coefficients()
 
             great_a = 1+(a/b)**2
-            great_b = -2*self.center[0]+2*(a/b)*(c/b+self.center[1])
-            great_c = self.center[0]**2+(c/b+self.center[1])**2-self.radius**2
+            great_b = -2*self._center[0]+2*(a/b)*(c/b+self._center[1])
+            great_c = self._center[0]**2+(c/b+self._center[1])**2-self._radius**2
 
             return great_b**2 >= 4*great_a*great_c
+        elif type(other) == Circle:
+            return squared_distance(self._center, other._center) <= (self._radius+other._radius)**2
+            
+class Rectangle(Shape):
+    def __init__(self, point_random: Point, width: float, height: float,angle=float):
+        self.angle=angle
+        self.origine = point_random
+        self.width = width
+        self.height = height
+    def get_points(self):
+        point_1=Point(self.origine[0]+self.width*sin(self.angle),self.origine[1]+self.width*cos(self.angle))
+        point_2=Point(point_1[0]+self.height*sin(self.angle-pi/2),point_1[1]+self.height*cos(self.angle-pi/2))
+        point_3=Point(self.origine[0]+self.height*sin(self.angle-pi/2),self.origine[1]+self.height*cos(pi/2-self.angle))
+
+        return (self.origine,point_1,point_2,point_3,self.origine[1])
+from geometry.vector import *
+from geometry.utils import squared_distance, barycenter
+
+class Shape:
+    def __init__(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, center: Point, radius: float):
+        assert(radius > 0)
+
+        self._center = center
+        self._radius = radius
+    
+    def get_center(self):
+        return self._center
+    
+    def get_radius(self):
+        return self._radius
+    
+    def set_center(self, center):
+        self._center = center
+    
+    def set_radius(self, radius):
+        assert(radius > 0)
+        self._radius = radius
+    
+    def __and__(self, other):
+        if type(other) == Line:
+            a, b, c = other.get_coefficients()
+
+            great_a = 1+(a/b)**2
+            great_b = -2*self._center[0]+2*(a/b)*(c/b+self._center[1])
+            great_c = self._center[0]**2+(c/b+self._center[1])**2-self._radius**2
+
+            return great_b**2 >= 4*great_a*great_c
+        elif type(other) == Circle:
+            return squared_distance(self._center, other._center) <= (self._radius+other._radius)**2
 
 class Rectangle(Shape):
     def __init__(self, point_random: Point, width: float, height: float,angle=float):
@@ -52,38 +105,16 @@ class Rectangle(Shape):
 
 class Triangle(Shape):
     def __init__(self, vertex1: Point, vertex2: Point, vertex3: Point):
-        self.vertices = [vertex1, vertex2, vertex3]
-
-def center(c: Circle):
-    return c.center
-
-def center(r: Rectangle):
-    c = Vector(r.top_left_corner)+Vector(r.width, r.height)/2
-    return Point(c)
-
-def center(t: Triangle):
-    return barycenter(t.vertices)
-
-def collision(c1: Circle, c2: Circle):
-    return (c1.radius+c2.radius)**2 <= squared_distance(c1.center, c2.center)
-
-def collision(c1:Rectangle,c2:Rectangle):
-    tlc1,width1,height1=c1.get()
-    points=[tlc1,(tlc1[0]+width1,tlc1[1]),(tlc1[0],tlc1[1]-height1),(tlc1[0]+width1,tlc1[1]+height1)]
-    tlc2,width2,height2=c2.get()
-
-    for i in points:
-        if tlc2[0]<=i[0]<=tlc2[0]+width2 and tlc2[1]-height1<=i[1]<=tlc2[1]:
-            return False
-
-def collision(r=Rectangle,c=Circle):
-    tlc1,width1,height1=r.get()
-    points=[tlc1,(tlc1[0]+width1,tlc1[1]),(tlc1[0]+width1,tlc1[1]+height1),(tlc1[0],tlc1[1]-height1),tlc1]
-    for i in range(4):
-        ligne=Line(points[i],points[i+1])
-        if intersection(ligne,c):
-            return True
-    return False
-
-def collision(c=Circle,r=Rectangle):
-    return collision(r,c)
+        self._vertices = [vertex1, vertex2, vertex3]
+    
+    def get_center(self):
+        return barycenter(self._vertices)
+    
+    def get_vertices(self):
+        return self._vertices
+    
+    def __getitem__(self, index):
+        return self._vertices[index]
+    
+    def __setitem__(self, index, vertex):
+        self._vertices[index] = vertex
