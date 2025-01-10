d08789acfc416e362c9cdc5a961f5875c7a90904
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-01-10 15:30:49 +0100

    utils.py

--- geometry/utils.py
+++ geometry/utils.py
@@ -1,19 +1,19 @@
 from geometry.vector import *
 from math import sqrt
 
-def squared_norm(p: Point):
-    return p.x**2+p.y**2
+def squared_norm(v: Vector):
+    return v[0]**2+v[1]**2
 
-def squared_distance(a: Point, b: Point):
-    return squared_norm(a-b)
+def squared_distance(A: Point, B: Point):
+    return squared_norm(create_vector(A, B))
 
-def norm(p: Point):
-    return sqrt(squared_norm(p))
+def norm(v: Vector):
+    return sqrt(squared_norm(v))
 
 def distance(a: Point, b: Point):
     return sqrt(squared_distance(a, b))
 
 def barycenter(points):
-    vectors = [Vector(p.x, p.y) for p in points] # convert points to vectors
+    vectors = [Vector(*p.get_coordinates()) for p in points] # convert points to vectors
     vectors_mean = sum(vectors, start = Vector(0, 0))/len(points) # mean of components
-    return Point(vectors_mean.components.x, vectors_mean.components.y)
+    return Point(*vectors_mean.get_components())
