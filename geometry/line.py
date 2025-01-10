686642f5c859bf876e89c90447104d37843a0d3d
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-01-10 18:54:10 +0100

    line.py

--- geometry/line.py
+++ geometry/line.py
@@ -1,8 +1,10 @@
 class Line:
     def __init__(self, p1, p2):
+        assert(p1 != p2)
+        
         self.a = p1[1]-p2[1]
         self.b = p2[0]-p1[0]
-        self.c = p1[1]*p2[0]-p1[0]*p2[1]
+        self.c = p1[0]*p2[1]-p1[1]*p2[0]
 
     def get_coefficients(self):
         return self.a, self.b, self.c
