0980141da2c8b9235011de73294b47d3f992d0e5
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-01-10 14:15:52 +0100

    vector.py
    Version finale.

--- geometry/vector.py
+++ geometry/vector.py
@@ -2,10 +2,24 @@ class Point:
     def __init__(self, x = 0., y = 0.):
         self.x = float(x)
         self.y = float(y)
+
+    def get_x(self):
+        return self.x
     
-    def __init__(self, v):
-        self.x = v.components.x
-        self.y = v.components.y
+    def get_y(self):
+        return self.y
+
+    def get_coordinates(self):
+        return (self.x, self.y)
+    
+    def set_x(self, x):
+        self.x = x
+    
+    def set_y(self, y):
+        self.y = y
+    
+    def set_coordinates(self, x, y):
+        self.x, self.y = x, y
     
     def __repr__(self):
         return "("+str(self.x)+", "+str(self.y)+")"
@@ -13,9 +27,24 @@ class Point:
 class Vector:
     def __init__(self, x = 0, y = 0):
         self.components = Point(x, y)
+
+    def get_x(self):
+        return self.components.get_x()
     
-    def __init__(self, p: Point):
-        self.components = p
+    def get_y(self):
+        return self.components.get_y()
+    
+    def get_components(self):
+        return self.components.get_coordinates()
+    
+    def set_x(self, x):
+        self.components.set_x(x)
+    
+    def set_y(self, y):
+        self.components.set_y(y)
+    
+    def set_components(self, x, y):
+        self.components.set_coordinates(x, y)
     
     def __add__(self, other):
         return Vector(self.components.x+other.components.x, self.components.y+other.components.y)
@@ -24,6 +53,9 @@ class Vector:
         return self+(other*(-1))
     
     def __mul__(self, scalar: float):
+        return Vector(self.components.x*scalar, self.components.x*scalar)
+    
+    def __rmul__(self, scalar: float):
         return Vector(self.components.x*scalar, self.components.x*scalar)
     
     def __div__(self, scalar: float):
