4d581109077a636c1de89e8084d7f8aeb0676be4
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-01-10 14:41:54 +0100

    vector.py
    Vraie version finale.

--- geometry/vector.py
+++ geometry/vector.py
@@ -3,48 +3,46 @@ class Point:
         self.x = float(x)
         self.y = float(y)
 
-    def get_x(self):
-        return self.x
-    
-    def get_y(self):
-        return self.y
-
     def get_coordinates(self):
         return (self.x, self.y)
     
-    def set_x(self, x):
-        self.x = x
-    
-    def set_y(self, y):
-        self.y = y
-    
-    def set_coordinates(self, x, y):
+    def set_cordinates(self, x, y):
         self.x, self.y = x, y
     
+    def __getitem__(self, index):
+        assert(index == 0 or index == 1)
+
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+    
+    def __setitem__(self, index, value):
+        assert(index == 0 or index == 1)
+
+        if index == 0:
+            self.x = value
+        elif index == 1:
+            self.y = value
+
     def __repr__(self):
         return "("+str(self.x)+", "+str(self.y)+")"
 
 class Vector:
     def __init__(self, x = 0, y = 0):
         self.components = Point(x, y)
-
-    def get_x(self):
-        return self.components.get_x()
-    
-    def get_y(self):
-        return self.components.get_y()
     
     def get_components(self):
         return self.components.get_coordinates()
     
-    def set_x(self, x):
-        self.components.set_x(x)
-    
-    def set_y(self, y):
-        self.components.set_y(y)
-    
     def set_components(self, x, y):
         self.components.set_coordinates(x, y)
+    
+    def __getitem__(self, index):
+        return self.components[index]
+    
+    def __setitem__(self, index, value):
+        self.components[index] = value
     
     def __add__(self, other):
         return Vector(self.components.x+other.components.x, self.components.y+other.components.y)
