e3636dee3d6f04a0ac5774220259c0a00189ab59
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-01-16 22:19:25 +0100

    Update utils.py

--- geometry/utils.py
+++ geometry/utils.py
@@ -4,16 +4,24 @@ from math import sqrt
 def squared_norm(v: Vector):
     return v[0]**2+v[1]**2
 
-def squared_distance(A: Point, B: Point):
-    return squared_norm(create_vector(A, B))
+def squared_distance(a: Vector, b: Vector):
+    return squared_norm(a-b)
 
 def norm(v: Vector):
     return sqrt(squared_norm(v))
 
-def distance(a: Point, b: Point):
+def distance(a: Vector, b: Vector):
     return sqrt(squared_distance(a, b))
 
 def barycenter(points: list):
-    vectors = [create_vector(p) for p in points] # convert points to vectors
-    vectors_mean = sum(vectors, start = Vector(0, 0))/len(points) # mean of components
-    return Point(*vectors_mean.get_components())
+    return sum(vectors, start = Vector(0, 0))/len(points) # mean of components
+
+def has_duplicates(l: list):
+    already_seen = set()
+    for x in l:
+        if x in already_seen:
+            return True
+
+        already_seen.add(x)
+
+    return False
