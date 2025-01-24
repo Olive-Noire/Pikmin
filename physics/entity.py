71e53a4b23a01609865648aa70146124a911137b
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-01-24 13:44:47 +0100

    Update entity.py

--- physics/entity.py
+++ physics/entity.py
@@ -2,8 +2,21 @@ from geometry.vector import Vector
 from geometry.shape import *
 
 class Entity:
-    def __init__(self, position: Vector, velocity: Vector, acceleration: Vector, collision_box: CollisionBox):
+    def __init__(self, position: Vector, velocity: Vector, collision_box: Shape, mass: float):
         self.position = position
         self.velocity = velocity
-        self.acceleraiton = acceleration
         self.collision_box = collision_box
+        self.mass = mass
+        self.forces = []
+    
+    def apply_force(self, vect):
+        self.forces.append(vect)
+
+    def update(self, dt):
+        acceleration = sum(self.forces, start = Vector(0, 0))*(1/self.mass)
+        self.velocity += dt*acceleration
+        self.position += dt*self.velocity
+    
+    def render(self):
+        if type(self.collision_box) == Circle:
+            # afficher un cercle
