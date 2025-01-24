74500278a1bb3b913f83e060ce4bcc86501ff910
TheOnlyPC <161340574+TheOnlyPC@users.noreply.github.com>
2025-01-24 13:55:42 +0100

    Delete old entity.py

--- entity.py
+++
@@ -1,9 +0,0 @@
-from vector import Vector
-from collisionbox import CollisionBox
-
-class Entity:
-    def __init__(self, position: Vector, velocity: Vector, acceleration: Vector, collision_box: CollisionBox):
-        self.position = position
-        self.velocity = velocity
-        self.acceleraiton = acceleration
-        self.collision_box = collision_box
