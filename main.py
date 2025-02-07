7a126118d364d5430d0544ada55d510498f3a649
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-02-07 14:47:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,30 @@
-import pygame, sys, random
-
-from physics.entity import Entity
-from geometry.vector import Vector
-from geometry.shape import Circle
-from geometry.utils import norm, squared_distance
-
+import pygame, sys
 pygame.init()
 
+from simulator.simulation import *
+from physics.entity import *
+
 screen = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
 
-dt = 1/30 # en secondes
-entities = [Entity(Vector(320, 240), Vector(0, 0), Circle(Vector(0, 0), 30), 1000000, color = (255, 255, 0))]
+s = Simulation(1, 1, True)
+s.set_state(True)
 
-add = lambda: entities.append(Entity(Vector(random.uniform(1, 640), random.uniform(1, 480)), Vector(random.uniform(1, 50), random.uniform(1, 50)), Circle(Vector(0, 0), 10), 10000, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))))
-
-while True:
-    screen.fill((0,0,0))
+while s.get_state():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()
+            s.set_state(False)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                add()
-                print(len(entities))
+                pass
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for e in s.get_entities().values():
+                if 
     
-    for cercle in entities:
-        c = cercle.position
-        for other in entities:
-            if squared_distance(cercle.position, other.position) > 3:
-                unitaire = other.position-c
-                unitaire = unitaire*(cercle.mass*other.mass/norm(unitaire)**3)
-
-                cercle.apply_force(unitaire)
-
-        cercle.update(dt)
-
-        center = (c[0], c[1])
-        pygame.draw.circle(screen, cercle.color, center, cercle.collision_box.get_radius())
+    s.update()
+    
+    screen.fill((0, 0, 0))
+    s.display_on(screen)
     
     pygame.display.update()
-    clock.tick(30)
+
+pygame.quit()
