a00920917fcf6cd29e442631623be793f69dddb5
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-02-22 00:24:33 +0100

    Update utils.py
    Ajout d'opérations sur les vecteurs et de la résolution d'équation du second degré.

--- geometry/utils.py
+++ geometry/utils.py
@@ -1,4 +1,4 @@
-from vector import *
+from geometry.vector import *
 
 from math import sqrt
 
@@ -15,7 +15,7 @@ def distance(a: Vector, b: Vector):
     return sqrt(squared_distance(a, b))
 
 def barycenter(points: list):
-    return sum(points, start = Vector(0, 0))/len(points) # mean of components
+    return sum(points, start = Vector(0, 0))*(1/len(points)) # mean of components
 
 def has_duplicates(l: list):
     already_seen = set()
@@ -26,3 +26,21 @@ def has_duplicates(l: list):
         already_seen.add(x)
 
     return False
+
+# Area of the parallelogram
+def determinant(a: Vector, b: Vector):
+    return a[0]*b[1]-a[1]*b[0]
+
+def dot_product(a: Vector, b: Vector):
+    return a[0]*b[0]+a[1]*b[1]
+
+def normalize(v: Vector):
+    return v/norm(v)
+
+def solve_quadratic_equation(a, b, c):
+    discriminant = b**2-4*a*c
+    if discriminant < 0:
+        return set()
+    else:
+        discriminant = sqrt(discriminant)
+        return {(-b-discriminant)/(2*a), (-b+discriminant)/(2*a)}
