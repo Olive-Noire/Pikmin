312cdb1e1ace307091cb3dbffa1b67684e93ffea
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-03-14 14:34:53 +0100

    Update entity.py

--- physics/entity.py
+++ physics/entity.py
@@ -2,22 +2,31 @@ from geometry.vector import Vector
 from geometry.shape import *
 
 class Entity:
-    def __init__(self, position: Vector, velocity: Vector, collision_box: Shape, mass: float):
-        self.position = position
-        self.velocity = velocity
-        self.collision_box = collision_box
-        self.mass = mass
-        self.forces = []
+    def __init__(self, velocity: Vector, collision_box: Shape, mass: float):
+        self._velocity = velocity
+        self._collision_box = collision_box
+        self._mass = mass
+        self._forces = []
     
     def apply_force(self, vect):
-        self.forces.append(vect)
+        self._forces.append(vect)
 
     def update(self, dt):
-        acceleration = sum(self.forces, start = Vector(0, 0))*(1/self.mass)
-        self.velocity += dt*acceleration
-        self.position += dt*self.velocity
+        acceleration = sum(self._forces, start = Vector(0, 0))*(1/self._mass)
+        self._velocity += dt*acceleration
+        self._collision_box.set_center(self._collision_box.get_center()+dt*self._velocity)
+
+        self._forces.clear()
     
-    def render(self):
-        if type(self.collision_box) == Circle:
-            pass
-            # afficher un cercle
+    def get_velocity(self):
+        return self._velocity
+    
+    def get_position(self):
+        return self.collision_box.get_center()
+    
+    def get_mass(self):
+        return self._mass
+
+    @property
+    def body(self):
+        return self._collision_box
