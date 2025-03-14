647d2f31b17ec620224b0efc00fa2369dd204b2e
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-03-14 15:14:29 +0100

    Update collisions.py

--- physics/collisions.py
+++ physics/collisions.py
@@ -1,8 +1,7 @@
 from geometry.shape import Circle, Polygon
-from geometry.utils import distance
+
 from geometry.vector import *
 from geometry.utils import *
-from math import pi
 
 def framing(fig):
     if type(fig) == Circle:
@@ -24,9 +23,9 @@ def framing(fig):
 
         return [(min_x, max_x), (min_y, max_y)]
 
-def sweep_and_prune(shapes): # one dimensional with insertion sort
-    frames_bank = [(framing(i), i) for i in shapes]
-    t = len(shapes)
+def sweep_and_prune(entities_dict): # one dimensional with insertion sort
+    frames_bank = [(framing(entities_dict[key].body), key) for key in entities_dict]
+    t = len(entities_dict)
     coll_to_check = []
 
     # On va trier par insertion car ce tri est optimal quand la liste est déjà quasi triée de base
@@ -46,34 +45,33 @@ def sweep_and_prune(shapes): # one dimensional with insertion sort
     liste_triee = [i[1] for i in frames_bank]
     return coll_to_check, liste_triee
 
-def static_resolution(obj1,obj2):
-    first,second=obj1.get_position(),obj2.get_position()
-    if type(first)==type(second)==Circle:
-        c1,c2,r1,r2=first.get_center(),second.get_center(),first.get_radius(),second.get_radius()
+def static_resolution(obj1, obj2):
+    if type(obj1) == type(obj2) == Circle:
+        c1,c2,r1,r2= obj1.get_center(), obj2.get_center(), obj1.get_radius(), obj2.get_radius()
         dist=(c1-c2)*(((r1+r2)/distance(c1,c2)-1))
-        first.set_center(c1+dist*(1/2))
-        second.set_center(c1+(-1)*dist*(1/2))
-        return (first,second)
 
-def dynamic_resolution(obj1,obj2):
-    first,second=obj1.get_position(),obj2.get_position()
-    v1,v2=obj1.get_velocity,obj2.get_velocity()
-    mass1,mass2=obj1.get_mass(),obj2.get_mass()
-    if type(first)==type(second)==Circle:
-        c1,c2,r1,r2=first.get_center(),second.get_center(),first.get_radius(),second.get_radius()
-        dist=distance(c1,c2)
-        normal=(c2-c1)*(1/dist)
-        x,y=normal.get_components()
-        tangeantal=Vector(-y,x)
-        dpt1=dot_product(v1,tangeantal)
-        dpt2=dot_product(v2,tangeantal)
-        dpn1=dot_product(v1,normal)
-        dpn2=dot_product(v2,normal)
-        second.set_center(c1+(-1)*dist*(1/2))
-        tx,ty=tangeantal.get_components()
+        return dist*(1/2), dist*(-1/2)
+
+def dynamic_resolution(ent1, ent2):
+    first, second = ent1.body, ent2.body
+    v1, v2 = ent1.get_velocity(), ent2.get_velocity()
+    mass1, mass2 = ent1.get_mass(), ent2.get_mass()
+    
+    if type(first) == type(second) == Circle:
+        c1, c2 = first.get_center(), second.get_center()
+        dist = distance(c1,c2)
+        normal = (c2-c1)*(1/dist)
+
+        x, y = normal.get_components()
+        tangeantal = Vector(-y, x)
+
+        dpt1 = dot_product(v1, tangeantal)
+        dpt2 = dot_product(v2, tangeantal)
+        dpn1 = dot_product(v1, normal)
+        dpn2 = dot_product(v2, normal)
 
         #conservation of momentum
-        m1=(dpn1*(mass1-mass2)+2*mass2*dpn2)/(mass2+mass1)
-        m2=(dpn2*(mass2-mass1)+2*mass1*dpn1)/(mass2+mass1)
-        obj1.apply_force(v1*dpt1+normal*m1)
-        obj2.apply_force(v2*dpt2+normal*m2)
+        m1 = (dpn1*(mass1-mass2)+2*mass2*dpn2)/(mass2+mass1)
+        m2 = (dpn2*(mass2-mass1)+2*mass1*dpn1)/(mass2+mass1)
+
+        return v1*dpt1+normal*m1, v2*dpt2+normal*m2
