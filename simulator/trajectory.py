f4db11fe553e03db0b86d4322cf180f95b6e6e33
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-04-11 14:25:12 +0200

    Update trajectory.py

--- simulator/trajectory.py
+++ simulator/trajectory.py
@@ -20,9 +20,17 @@ class Trajectory:
         else:
             return False
     
+    def translate(self, translation):
+        for i in range(len(self._points)):
+            self._points[i] += translation
+    
+    def zoom(self, z):
+        for i in range(len(self._points)):
+            self._points[i] = Vector((self._points[i][0]-600)*z+600, (self._points[i][1]-350)*z+350)
+    
     def draw(self, surface, camera):
         if len(self._points) >= 2:
-            vect_to_tuple = lambda v: (v[0]+camera.position[0], camera.position[1]-v[1])
+            vect_to_tuple = lambda v: (v[0], -v[1])
 
             # à optimiser parce que y a deux boucles
             draw.lines(surface, (0, 255, 0), False, [vect_to_tuple(position) for position in self._points], 2)
