3ec9e6a8b97d7234e2644eca17816810ecfb5171
Olive <69757487+Olive-Noire@users.noreply.github.com>
2025-04-11 15:30:59 +0200

    Update entity.py

--- physics/entity.py
+++ physics/entity.py
@@ -5,6 +5,10 @@ class Entity:
     def __init__(self, velocity: Vector, collision_box: Shape, mass: float):
         self._velocity = velocity # m.s^-1
         self._collision_box = collision_box
+
+        self.angular_velocity = 0
+        self.angle = 0
+
         self._mass = mass # Kg
         self._forces = [] # N
     
@@ -15,13 +19,17 @@ class Entity:
         return self._forces
 
     def update(self, dt):
-        #print(self._velocity)
-
-        acceleration = sum(self._forces, start = Vector(0, 0))*(1/self._mass)
+        acceleration = sum(self._forces, start = Vector(0, 0))*(1/self._mass) # m.s^-2
         self._velocity += dt*acceleration
         self._collision_box.set_center(self._collision_box.get_center()+dt*self._velocity)
 
+        if type(self.body) == Polygon:
+            self.body.rotate(self.angular_velocity*dt)
+
         self._forces.clear()
+    
+    def add_angular_velocity(self, ac):
+        self.angular_velocity += ac
     
     def get_velocity(self):
         return self._velocity
